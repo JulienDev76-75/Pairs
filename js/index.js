934cbe86ff03fbd6af97f4c0c0d65e389f66a592
JulienDev76-75 <julien.coart@gmail.com>
2021-04-16 18:07:08 +0200

    comparaison working

--- js/index.js
+++ js/index.js
@@ -1,20 +1,58 @@
 //************** variable globales ***********
-let cardColors =  ["red","yellow", "green", "pink", "purple", "blue", "red","yellow", "green", "pink", "purple", "blue"];
+let cardColors =  ["red","yellow", "green", "pink", "purple", "blue", "brown", "red","yellow", "green", "pink", "purple", "blue", "brown"];
 let cardAssociate = [];
+let score = 0;
 
 let card = document.getElementsByClassName("card");
-
-for(var i = 0;i < card.length;i++){
+for(let i = 0;i < card.length;i++) {
     card[i].onclick= function(){
-        card.style.backgroundColor = "yellow";
-        card.innerHTML = "YOU CLICKED ME";
+        card[i].classList.remove("bg-dark");
+        card[i].style.backgroundColor = cardColors[i];
+        cardAssociate.push(card[i]);
+
+        Intervalles ();
+    }
+};
+
+function Intervalles () {
+    if (cardAssociate.length === 2) {
+        setTimeout(function () {comparaison()}, 1500);
+    }
+    else if (cardAssociate.length > 2){
+        alert("Ne choisissez que deux cartes");
     };
 }
 
-//let card = document.getElementsByClassNames("card");
-//card.addEventListener("click", function () {
-//    card.style.backgroundColor = "yellow";
-//});
+ function Comparaison () {
+    if (cardAssociate[0].style.backgroundColor === cardAssociate[1].style.backgroundColor){
+        console.log(cardAssociate);   
+        alert("les deux cartes sont identiques");
+        score += 1;
+        cardAssociate.length = 0;
+    }
+    else {
+        alert("les deux cartes ne sont pas identiques");
+        score += 0;
+
+    };
+}
+//     function choixPlayer() {
+//     if (cardAssociate[0] === cardAssociate[1]) {
+//         card[i].style.visibility = "visible";
+//           score ++;
+//         }
+//     else {
+//         card[i].classList.replace("bg-dark");
+//         }
+// };
+
+// function intervalles() {
+//     setInterval(function(){}, 3000);
+
+
+
+
+
 
 
 

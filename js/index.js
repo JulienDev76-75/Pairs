4b21759cbd546a80b87a97e6d43d7b12c72c5235
JulienDev76-75 <julien.coart@gmail.com>
2021-04-18 06:19:12 +0200

    double clic on the same card problem

--- js/index.js
+++ js/index.js
@@ -1,18 +1,25 @@
 //************** variable globales ***********
+
 let cardColors =  ["red","yellow", "green", "pink", "purple", "blue", "brown", "red","yellow", "green", "pink", "purple", "blue", "brown"];
 let cardAssociate = [];
 let score = 0;
-let timeleft = 30;
+let timeleft = 40;
 let card = document.getElementsByClassName("card");
 let countDownDate = document.getElementById("timer");
 
+//************** functions ***********
+
+//The player push on button to activate the timer function
 function timer () {
     let downloadTimer = setInterval(function(){
+    //every second, the timeleft decrease until 0
     timeleft--;
     countDownDate.textContent = timeleft;
-    if(timeleft <= 0)
+    //if timeleft <= 0, the game restart and timer too
+    if(timeleft <= 0) {
         clearInterval(downloadTimer);
-        card[i].classList.remove("bg-dark");
+        EndofGame();
+    }
     },1000);
 }
 //Apply the Fisher-Yates method to "randomize" cardColors Array
@@ -30,9 +37,8 @@ function shuffle(array) {
     }
     return array;
 };
-shuffle(cardColors);
 
-//Gme is beggining when the player pick a card
+//Game is beggining when the player pick a card
 for(let i = 0;i < card.length;i++) {
     card[i].onclick= function(){
         card[i].classList.remove("bg-dark");
@@ -43,11 +49,14 @@ for(let i = 0;i < card.length;i++) {
 };
 
 function Intervalles () {
+    //if the player is chosing 2 cards only, cardAssociate contains 2 items, comparaison start
     if (cardAssociate.length === 2) {
         setTimeout(function () {comparaison()}, 1100);
     }
+    //if the player is chosing more than 2 cards cardAssociate will have 3 items, so this condition is applied
      else if (cardAssociate.length > 2){
          alert("Ne choisissez que deux cartes");
+         score += 0;
          cardAssociate[0].classList.add("bg-dark");
          cardAssociate[1].classList.add("bg-dark");
          cardAssociate[2].classList.add("bg-dark");
@@ -55,21 +64,19 @@ function Intervalles () {
 }
 
  function comparaison () {
+    //if style.background items in CardAssociate array are same
     if (cardAssociate[0].style.backgroundColor === cardAssociate[1].style.backgroundColor){
+        //the victory is defined by score declared as variable upside, score start at zero and increase if pairs are found
         score += 1;
+        //we empty the cardAssociate array, if we don't do that : we can't continue to compare cards, because the array contains too much items (more than 2)
         cardAssociate.length = 0;
         findWinner(); 
     }
-    // else if (cardAssociate[0].style.backgroundColor === cardAssociate[0].style.backgroundColor) {
 
-    //     score += 0;
-    //     alert("c'est complètement idiot de sélectionner la même carte");
-    //     cardAssociate.length = 0;
-        
-    // }
     else {
         for(let i = 0;i < cardAssociate.length;i++) {
         score += 0;
+        //if the pairs are not same, so cards will recover his bg-dark
         cardAssociate[0].classList.add("bg-dark");
         cardAssociate[1].classList.add("bg-dark");
         cardAssociate.length = 0;
@@ -80,28 +87,39 @@ function Intervalles () {
 function findWinner(){
     if (score === 7){
         alert ("bravo vous avez gagné !");
+        //the function is avoided to start
+        timeleft = 40;
     };
 }
 
+function EndofGame ()
+{
+    if (timeleft === 0 && score <7){
+        alert ("*soupir*loser");
+        document.location.reload();
+    }
+}
 
-// function timer() {
-//     let seconds = 45;
-//     let timer = document.getElementById("timer");
-//     timer.innerHTML = "<i class='fas fa-hourglass-start ml-2'></i> Temps restant : " + seconds + " s";
-//     setInterval(function(){
-//       if(seconds >= 1) {
-//         seconds --;
-//         timer.innerHTML = "<i class='fas fa-hourglass-start ml-2'></i> Temps restant : " + seconds + " s";
-//       }
-//       else {
-//         restartGame(false);
-//       }
-//     }, 1000);
-//   }
+//************** Code ***********
+
+shuffle(cardColors);
+
+//Game is beggining when the player pick a card
+for(let i = 0;i < card.length;i++) {
+    card[i].onclick= function(){
+        card[i].classList.remove("bg-dark");
+        card[i].style.backgroundColor = cardColors[i];
+        cardAssociate.push(card[i]);
+        Intervalles ();
+    }
+};
+
+//if the player appuie sur Jouer alors il peut jouer, sinon les cards sont en display : none
 
 
 
-// Set the date we're counting down to
+
+
 
 
 
